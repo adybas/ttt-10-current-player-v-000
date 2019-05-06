ce843a32074877461d298a3880ca25f36cec0aca
Anna Dybas <a.m.dybas@gmail.com>
2019-05-06 17:42:35 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,12 @@ def turn_count(board)
 
   number_of_turns = 0
 
-  board.each do | |
-
-    number_of_turns += 1
+  board.each do |token|
+    if token === "X" || token === "O"
+      number_of_turns += 1
+    else
+    
+    end
   end
-  
+
 end
